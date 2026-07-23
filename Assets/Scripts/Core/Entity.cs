894e17de5b52f67c9228122b3c6ba6e5a3ac60dd
oblivorten <oblivorten@gmail.com>
2026-07-24 01:32:15 +0300

    Фикс системы здоровья

--- Assets/Scripts/Core/Entity.cs
+++ Assets/Scripts/Core/Entity.cs
@@ -3,33 +3,11 @@ using UnityEngine;
 public abstract class Entity : MonoBehaviour
 {
     [SerializeField] private Team _team;
-    [SerializeField] private int _maxHealth = 100;
 
-    public int CurrentHealth { get; protected set; }
     public Team Team => _team;
 
     protected virtual void Awake()
     {
-        CurrentHealth = _maxHealth;
-    }
 
-    public virtual void TakeDamage(int damage)
-    {
-        if (damage <= 0) {
-            return;
-        }
-
-        CurrentHealth -= damage;
-        CurrentHealth = Mathf.Max(CurrentHealth, 0);
-
-        if (CurrentHealth <= 0)
-        {
-            Die();
-        }
-    }
-
-    protected virtual void Die()
-    {
-        Destroy(gameObject);
     }
 }
